36ab4f2e8db91eaba3bd3fc004165d7f24bea540
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2019-04-05 17:20:28 +0700

    Echo back audio for debuging

--- server-streaming/src/services/stream/audio_stream_service.cpp
+++ server-streaming/src/services/stream/audio_stream_service.cpp
@@ -84,8 +84,8 @@ void AudioStreamService::sendAudio(unsigned char source_client_id, const std::ve
     for (int i = 0; i < conference.participants.size(); ++i) {
 
         // Send audio to client if connected
-        if (conference.participants[i].isConnectedAudio() && conference.participants[i].getClientId() != source_client_id) {
-
+        // if (conference.participants[i].isConnectedAudio() && conference.participants[i].getClientId() != source_client_id) {
+        if (conference.participants[i].isConnectedAudio()) {
             std::vector<unsigned char> message = protocol_data.packageConferenceAudioFrame(conference.participants[i].getClientId(), data);
             socket->sendPackage(conference.participants[i].getClientAudioAddress(), message);
 
